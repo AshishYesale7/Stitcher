29ba84a0922014f85eb14c7fc24898e8bb56585d
Ashish Yesale <ashishyesale007@gmail.com>
2025-09-14 19:01:59 +0000

    so we have to add environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "studio-6550566511-bfe33",
-  "appId": "1:573585068434:web:2b5e3c63156861df17f14b",
-  "storageBucket": "studio-6550566511-bfe33.firebasestorage.app",
-  "apiKey": "AIzaSyCatApKbk4OYVRb9GB7rpaMs8_oOcvL9Jc",
-  "authDomain": "studio-6550566511-bfe33.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "573585068434"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
