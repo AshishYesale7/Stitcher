a9e98363a71f1de0df6fbd5182ff5ea65ea54de1
Ashish Yesale <ashishyesale007@gmail.com>
2025-09-14 17:17:16 +0000

    make this size smaller (_for element h1_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-6 text-center">
-              <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter font-headline bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent">
+              <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold tracking-tighter font-headline bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent">
                 Your Perfect Fit, Found.
               </h1>
               <p className="max-w-[700px] text-muted-foreground md:text-xl">
